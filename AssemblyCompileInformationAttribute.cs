7d34d4d51decd46873fbc9dbd024aadd9eb3c9b6
obacht <obacht@7e40c986-1927-0410-85fd-b658cee3587f>
2007-01-15 21:30:25 +0000

    initial testing completed, deleted more autogen-files

--- AssemblyCompileInformationAttribute.cs
+++
@@ -1,9 +0,0 @@
-namespace System.Reflection
-{
-	[AttributeUsage(AttributeTargets.Assembly,AllowMultiple=false)]
-	class AssemblyCompileInformationAttribute : Attribute
-	{
-		public string CompileDate {get{return "2007-01-15";}}
-		public string CompileTime {get{return "22:15:47";}}
-	}
-}
